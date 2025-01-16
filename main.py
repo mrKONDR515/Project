4e7d81622f3686732688c77e9809c977fc599ece
AtlasSs01 <86181516+AtlasSs01@users.noreply.github.com>
2025-01-16 19:49:54 +0400

    11

--- main.py
+++ main.py
@@ -1,67 +1,161 @@
 import pygame
-import time
+import sys
+import os
 
 pygame.init()
-size = WIDTH, HEIGHT = 800, 600
+size = WIDTH, HEIGHT = 900, 600
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 FPS = 50
 
-CENTER_X = WIDTH / 2
-CENTER_Y = HEIGHT / 2
-game_over = False
-finalized = False
-garden_happy = True
-fangflower_collision = False
-time_elapsed = 0
-start_time = time.time()
-flower_list = []
-wilted_list = []
-fangflower_list = []
-fangflower_vy_list = []
-fangflower_vx_list = []
 
-
-def draw():
-    global game_over, time_elapsed, finalized
-    if not game_over:
-        # screen.clear() это что XD
-        screen.blit(pygame.image.load("garden.png"), (0, 0))
-        screen.blit(pygame.image.load("cow.jpg"), (100, 500))
-
-        for flower in flower_list:
-            flower.draw()
-        for fangflower in fangflower_list:
-            fangflower.draw()
-
-        time_elapsed = int(time.time() - start_time)
-        '''font = pygame.font.Font(None, 36)
-        text = font.render("Garden happy for: " + str(time_elapsed) + " seconds", True, (0, 0, 0))
-        screen.blit(text, (10, 10))'''
-        screen.draw.text("Garden happy for: " + str(time_elapsed) + " seconds",
-                         topleft=(10, 10), color="black")
-
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
     else:
-        if not finalized:
-            screen.blit(pygame.image.load("cow.jpg"), (100, 500))
-            screen.draw.text(
-                "Garden happy for: " + str(time_elapsed) + " seconds",
-                topleft=(10, 10), color="black")
-
-            '''if not garden_happy:
-                screen.draw.text("GARDEN UNHAPPY - GAME OVER!")
-            else:
-                screen.draw.text("FANGFLOWER ATTACK - GAME OVER!")
-            finalized = True'''
-
-    pygame.display.flip()
+        image = image.convert_alpha()
+    return image
 
 
+def load_level(filename):
+    filename = "data/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+tile_images = {
+    'flower': load_image('flower.jpg'),
+    'empty': load_image('grass.png')
+}
+
+tile_width = tile_height = 50
+tiles_group = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = load_image('cow.jpg', -1)
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
+        self.pos = (pos_x, pos_y)
+
+    def move(self, x, y):
+        self.pos = (x, y)
+        self.rect = self.image.get_rect().move(
+            tile_width * self.pos[0] + 15, tile_height * self.pos[1] + 5)
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('flower', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+    # вернем игрока, а также размер поля в клетках
+    return new_player, x, y
+
+
+def move_player(player, direction):
+    x, y = player.pos
+    if direction == 'up':
+        if y > 0 and level[y - 1][x] in ['.', '@']:
+            player.move(x, y - 1)
+    elif direction == 'down':
+        if y < level_y - 1 and level[y + 1][x] in ['.', '@']:
+            player.move(x, y + 1)
+    elif direction == 'left':
+        if x > 0 and level[y][x - 1] in ['.', '@']:
+            player.move(x - 1, y)
+    elif direction == 'right':
+        if x < level_x - 1 and level[y][x + 1] in ['.', '@']:
+            player.move(x + 1, y)
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ["ЗАСТАВКА", "",
+                  "Правила игры",
+                  "Если в правилах несколько строк,",
+                  "приходится выводить их построчно"]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+level = load_level('map1.txt')
+player, level_x, level_y = generate_level(level)
+start_screen()
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    draw()
+            terminate()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                move_player(player, 'up')
+            if event.key == pygame.K_DOWN:
+                move_player(player, 'down')
+            if event.key == pygame.K_LEFT:
+                move_player(player, 'left')
+            if event.key == pygame.K_RIGHT:
+                move_player(player, 'right')
+    screen.fill(pygame.Color('black'))
+    all_sprites.draw(screen)
+    player_group.draw(screen)
+    pygame.display.flip()
     clock.tick(FPS)
-pygame.quit()
